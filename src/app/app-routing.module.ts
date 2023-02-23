5e163e41da164002ad4bf17beafe29f83aa0726e
Sumit Arora <er.sumitarora@gmail.com>
2023-02-22 19:23:29 -0500

    feat: adding sub routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { RouteOneComponent } from './components/route-one/route-one.component';
+import { RouteThreeComponent } from './components/route-three/route-three.component';
+import { RouteTwoComponent } from './components/route-two/route-two.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'route-one', component: RouteOneComponent },
+  { path: 'route-two', component: RouteTwoComponent },
+  { path: 'route-three', component: RouteThreeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RouteOneComponent } from './components/route-one/route-one.component';
+import { RouteTwoComponent } from './components/route-two/route-two.component';
+import { RouteThreeComponent } from './components/route-three/route-three.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RouteOneComponent,
+    RouteTwoComponent,
+    RouteThreeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
