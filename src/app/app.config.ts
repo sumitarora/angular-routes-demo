e043e96e135cf5321789fe4792875df09223672e
Sumit Arora <er.sumitarora@gmail.com>
2025-02-18 12:54:15 -0500

    fix: fixing build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideRouter(routes),
+  ],
 };
